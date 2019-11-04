00400ee269e1879896e2deea55cd97ddb35605bf
Brandon Garsson <s1293728@monmouth.edu>
2019-11-04 17:23:08 -0500

    Update pySumAverage.py

--- pySumAverage.py
+++ pySumAverage.py
@@ -3,13 +3,14 @@
 #s1293728
 
 x=1
-sum=0
+numbers=[]
 
 while x<=20:
     n=input("Enter a number: ")
+    numbers.append(n)
     x=x+1
-    sum=sum+int(n)
 
+sum=int(numbers[0])+int(numbers[1])+int(numbers[2])+int(numbers[3])+int(numbers[4])+int(numbers[5])+int(numbers[6])+int(numbers[7])+int(numbers[8])+int(numbers[9])+int(numbers[10])+int(numbers[11])+int(numbers[12])+int(numbers[13])+int(numbers[14])+int(numbers[15])+int(numbers[16])+int(numbers[17])+int(numbers[18])+int(numbers[19])
 print("\nThe sum of the numbers you entered is: "+str(sum))
 print("The average of the numbers you entered is: "+str((float(sum)/20)))
 
